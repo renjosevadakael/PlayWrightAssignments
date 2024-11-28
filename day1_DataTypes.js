0211b0c38e9c49ecaf96ec322ca91a0761e9135e
renjosevadakael <renju.joseph.jose@gmail.com>
2024-11-28 17:43:18 +0530

    Add files via upload
    Day 1 Assignmentt

---
+++ day1_DataTypes.js
@@ -0,0 +1,28 @@
+/*Assignment Details:  
+Declare variables using `let` for different data types in JavaScript, and verify their types
+ using `typeof`, including an uninitialized variable. 
+ 
+Assignment Requirements:  
+    Create the following variables using let (not using var) and check their typeOf 
+    a) firstName 
+    b) companyName 
+    c) mobileNumber 
+    d) isAutomation 
+    e) hasPlaywright (do not assign) 
+ Print and confirm the values and data types */
+
+ let firstName=`Renju`;
+ let companyName='Cognizant';
+ let mobileNumber=7560829071;
+ let isAutomation=true;
+let hasPlaywright;
+console.log(`firstName---` +firstName);
+console.log(`firstName typeOf---` +typeof firstName);
+console.log(`companyName---` +companyName);
+console.log(`companyName typeOf---` +typeof companyName);
+console.log(`mobileNumber---` +mobileNumber);
+console.log(`mobileNumber typeOf---` +typeof mobileNumber);
+console.log(`isAutomation---` +isAutomation);
+console.log(`isAutomation typeOf---` +typeof isAutomation);
+console.log(`hasPlaywright---` +hasPlaywright);
+console.log(`hasPlaywright typeOf---` +typeof hasPlaywright);
