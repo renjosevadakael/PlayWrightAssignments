3257579623196298897612b4c26396e1ba0b932c
renjosevadakael <renju.joseph.jose@gmail.com>
2025-01-03 18:34:43 +0530

    Add files via upload

--- day9/03radiobuttonassignment.spec.ts
+++ day9/03radiobuttonassignment.spec.ts
@@ -11,17 +11,35 @@ import {  expect , test } from "playwright/test";
 
 test(` Test Radio Button functionalities` , async ({page}) => { 
 page.goto("https://leafground.com/radio.xhtml");
-const defaultSelRadioBtn= page.locator("//h5[text()='Find the default select radio button']/following-sibling::div//label[text()='Safari']")
-await expect(defaultSelRadioBtn).toContainText(`Safari`);
-// Qn- How to locate this easier - with filter?
+// Qn- How to locate this easier - with filter? - done with locator and filters
+//const defaultSelRadioBtn= page.locator("//h5[text()='Find the default select radio button']/following-sibling::div//label[text()='Safari']")
+const selectDefaultRadioBtn=  page.locator(".card").filter({hasText : "Find the default select radio button"});
+const browserSelected= selectDefaultRadioBtn.locator("//label[text()='Safari']/preceding::input[1]")
+await expect( browserSelected).toBeChecked();
+const safari = selectDefaultRadioBtn.locator("//label[text()='Safari']");
+await expect(safari).toContainText(`Safari`);
 
-await page.locator("//h5[text()='Your most favorite browser']/parent::div//label[text()='Chrome']/preceding-sibling::div//div[@class='ui-radiobutton-box ui-widget ui-corner-all ui-state-default']").click();
+//Click your most favorite browser and assert that the browser is enabled.
+//await page.locator("//h5[text()='Your most favorite browser']/parent::div//label[text()='Chrome']/preceding-sibling::div//div[@class='ui-radiobutton-box ui-widget ui-corner-all ui-state-default']").click();
+const favBrowserRadioBtn= page.locator(".card").filter({hasText: "Your most favorite browser"});
+await favBrowserRadioBtn.locator("//label[text()='Chrome']/preceding::div[1]").click();
+const favBroswerSelected = favBrowserRadioBtn.locator("//label[text()='Chrome']/preceding::input[1]");
+await expect(favBroswerSelected).toBeChecked();
 
 //Click one of the cities. 
 await page.locator("//label[text()='Chennai']/preceding-sibling::div").click();
-
-await page.locator("//label[text()='1-20 Years']/preceding-sibling::div/div[@class='ui-radiobutton-box ui-widget ui-corner-all ui-state-default']").click();
 // Qn How to Select the age group. Assert the default selected button.?
+// 1. Assert the default selected button, then select a different age group and again assert it
+const selectDefaultAgeGrpRadioBtn= page.locator(".card").filter({hasText :"Select the age group (only if not selected)"});
+const  ageGroupSelected = selectDefaultAgeGrpRadioBtn.locator("//label[text()='21-40 Years']/preceding::input[1]")
+await expect(ageGroupSelected).toBeChecked();
+await selectDefaultAgeGrpRadioBtn.locator("//label[text()='1-20 Years']/preceding::div[1]").click();
+const  newAgeGroupSelected = selectDefaultAgeGrpRadioBtn.locator("//label[text()='1-20 Years']/preceding::input[1]");
+await expect(newAgeGroupSelected).toBeChecked();
+
+
+//await page.locator("//label[text()='1-20 Years']/preceding-sibling::div/div[@class='ui-radiobutton-box ui-widget ui-corner-all ui-state-default']").click();
+
 //const ageRadioBtn= page.locator("//label[text()='1-20 Years']/preceding-sibling::div/div[@class='ui-radiobutton-box ui-widget ui-corner-all ui-state-default ui-state-active']");
 
 
@@ -29,7 +47,7 @@ await page.locator("//label[text()='1-20 Years']/preceding-sibling::div/div[@cla
 //console.log(isChecked)
 //console.log('Radio button is checked:', isChecked); 
 //expect(isChecked).toBe(true);
- 
+// Shift+Alt+a = Block Comment 
 }
     
 
